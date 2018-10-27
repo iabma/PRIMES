5afca577deb28836ef76eef5006a9afbe295d91a
Ian Balaguera <balagueraian@gmail.com>
2018-10-27 12:27:35 -0400

    Created an answer checker for G1.
    It runs through a set number of iterations (default 10 million) to acquire a rough probability for a given number of desired pairs.

---
+++ src/G1AnswerCheck.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class G1AnswerCheck {
+    private static final int NUM_ITERATIONS = 10000000;
+    private static final int PAIR_AMT = 2;
+
+    public static void main(String[] args) {
+        Random gen = new Random();
+        int numSuccesses = 0;
+
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
+            int[] set = new int[10];
+            boolean amtReached = false;
+            int numPairs = 0;
+            for (int j = 0; j < 10; j++) {
+                int val = gen.nextInt(2);
+                set[j] = val;
+                if (j > 0 && set[j] == set[j-1]) numPairs++;
+                amtReached = numPairs == PAIR_AMT;
+            }
+            if (amtReached) numSuccesses++;
+        }
+
+        System.out.printf("%.3f%%" ,numSuccesses / (double) NUM_ITERATIONS * 100.0);
+    }
+}
