1265e280c3a5f12d29f45b1f0b0eab78af9dc8be
Ian Balaguera <balagueraian@gmail.com>
2018-10-27 20:12:30 -0400

    Added a solution check for G3.b.

--- src/G3AnswerCheck.java
+++ src/G3AnswerCheck.java
@@ -1,13 +1,11 @@
 import java.util.Random;
 
 public class G3AnswerCheck {
-    private static final long NUM_ITERATIONS = 10000000l;
+    private static final long NUM_ITERATIONS = 10000l;
 
-    public static void main(String[] args) {
+    private static void checkA() {
         Random gen = new Random();
         long netTime = 0;
-
-
 
         for (long i = 0; i < NUM_ITERATIONS; i++) {
             int pos = 1;
@@ -24,9 +22,38 @@ public class G3AnswerCheck {
                 netTime++;
                 numMoves++;
             }
-            //System.out.println(numMoves + " moves");
+            System.out.println(numMoves + " moves");
         }
 
         System.out.printf("%.5f minutes" , netTime / (double) NUM_ITERATIONS);
+    }
+
+    private static void checkB() {
+        Random gen = new Random();
+        long netTime = 0;
+
+        for (long i = 0; i < NUM_ITERATIONS; i++) {
+            int pos = 1;
+            int numMoves = 0;
+            while (pos != 8) {
+                int move = gen.nextInt(3);
+                if (pos == 1) pos = move == 0 ? 2 : move + 3;
+                else if (pos == 2) pos = move == 1 ? 7 : move + 1;
+                else if (pos == 3) pos = move == 1 ? 8 : move + 2;
+                else if (pos == 4) pos = move == 1 ? 6 : move + 1;
+                else if (pos == 5) pos = move == 0 ? 1 : move + 5;
+                else if (pos == 6) pos = move == 2 ? 8 : move + 4;
+                else if (pos == 7) pos = move == 0 ? 5 : move == 1 ? 8 : move;
+                netTime++;
+                numMoves++;
+            }
+            System.out.println(numMoves + " moves");
+        }
+
+        System.out.printf("%.5f minutes" , netTime / (double) NUM_ITERATIONS);
+    }
+
+    public static void main(String[] args) {
+        checkB();
     }
 }
